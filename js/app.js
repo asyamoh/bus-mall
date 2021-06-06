ab77cb7663458091ab9adb29facd310ae2b7f2ef
asyamoh <asia_nazif@yahoo.com>
2021-06-06 21:52:43 +0300

    button

--- js/app.js
+++ js/app.js
@@ -1,143 +1,105 @@
 'use strict';
 
-
 let leftImageElement = document.getElementById('left-image');
+let middleImageElement = document.getElementById('middle-image');
 let rightImageElement = document.getElementById('right-image');
-let midimageElemnt = document.getElementById('mid-image');
-let container = document.getElementById('section-one');
-////
-let counts = 0;
-let maxAttempts = 25;
-let leftIndex=0; 
-let rightIndex=0;
-let midindex=0;
-let arrOfnames = [];
+let maxAttempts =25;
+let userAttemptsCounter = 0;
+let leftImageIndex;
+let middleImageIndex;
+let rightImageIndex;
 
-
-function product(name,source){
-  this.name= name;
+function Product(name, source, timesShown) {
+  this.name = name;
   this.source = source;
-  this.time = 0;
+  this.timesShown = timesShown;
+  this.timesShown = 0;
   this.votes = 0;
-  product.allImages.push(this);
-  arrOfnames.push(this.name);
+  Product.allProducts.push(this);
+}
+
+Product.allProducts = [];
+new Product('bag', 'images/bag.jpg');
+new Product('banana', 'images/banana.jpg');
+new Product('bathroom', 'images/bathroom.jpg');
+new Product('boots', 'images/boots.jpg');
+new Product('breakfast', 'images/breakfast.jpg');
+new Product('bubblegum', 'images/bubblegum.jpg');
+new Product('chair', 'images/chair.jpg');
+new Product('cthulhu', 'images/cthulhu.jpg');
+new Product('dog-duck', 'images/dog-duck.jpg');
+new Product('dragon', 'images/dragon.jpg');
+new Product('pen', 'images/pen.jpg');
+new Product('pet-sweep', 'images/pet-sweep.jpg');
+new Product('scissors', 'images/scissors.jpg');
+new Product('shark', 'images/shark.jpg');
+new Product('sweep', 'images/sweep.png');
+new Product('tauntaun', 'images/tauntaun.jpg');
+new Product('unicorn', 'images/unicorn.jpg');
+new Product('usb', 'images/usb.gif');
+new Product('water-can', 'images/water-can.jpg');
+new Product('wine-glass', 'images/wine-glass.jpg');
+
+function creatingRandomIndex() {
+  return Math.floor(Math.random() * Product.allProducts.length);
+}
+
+creatingRandomIndex();
+console.log(creatingRandomIndex());
+
+function renderThreeImages() {
+  leftImageIndex = creatingRandomIndex();
+  middleImageIndex = creatingRandomIndex();
+  rightImageIndex = creatingRandomIndex();
+  while (leftImageIndex === middleImageIndex || leftImageIndex === rightImageIndex) {
+    rightImageIndex = creatingRandomIndex();
+    middleImageIndex = creatingRandomIndex();
+  }
+  while (rightImageIndex === middleImageIndex) {
+    middleImageIndex = creatingRandomIndex();
+  }
+  leftImageElement.src = Product.allProducts[leftImageIndex].source;
+  Product.allProducts[leftImageIndex].timesShown++;
+  rightImageElement.src = Product.allProducts[rightImageIndex].source;
+  Product.allProducts[rightImageIndex].timesShown++;
+  middleImageElement.src = Product.allProducts[middleImageIndex].source;
+  Product.allProducts[middleImageIndex].timesShown++;
+
 
 }
 
+renderThreeImages();
 
-product.allImages =[];
-
-
-new product('bag','../img/bag.jpg');
-new product('banana','../img/banana.jpg');
-new product('bathroom','../img/bathroom.jpg');
-new product('boots','../img/boots.jpg');
-new product('breakfast','../img/breakfast.jpg');
-
-new product('bubblegum','../img/bubblegum.jpg');
-new product('chair','../img/chair.jpg');
-new product('cthulhu','../img/cthulhu.jpg');
-new product('dog-duck','../img/dog-duck.jpg');
-new product('dragon','../img/dragon.jpg');
-
-new product('pen','../img/pen.jpg');
-
-new product('scissors','../img/scissors.jpg');
-new product('shark','../img/shark.jpg');
-new product('sweep','../img/sweep.png');
-
-new product('tauntaun','../img/tauntaun.jpg');
-new product('unicorn','../img/unicorn.jpg');
-new product('usb','../img/usb.gif');
-new product('water-can','../img/water-can.jpg');
-new product('glass','../img/wine-glass.jpg');
-
-
-console.log(product.allImages);
-
-
-
-function renderthreeImages(){
-
-  
-    leftIndex = genrateRandomIndex(); 
-    midindex =  genrateRandomIndex();
-    rightIndex = genrateRandomIndex();  
-    let arrOfindex=[]
-
-  
-    while(leftIndex === rightIndex || leftIndex === midindex || rightIndex === midindex 
-      || arrOfindex.includes(leftIndex)|| arrOfindex.includes(rightIndex)|| arrOfindex.includes(midindex)){
-      leftIndex = genrateRandomIndex();
-      rightIndex = genrateRandomIndex();
-      midindex =  genrateRandomIndex();
+let container = document.getElementById('container');
+container.addEventListener('click', handleUserClick);
+function handleUserClick(event) {
+  userAttemptsCounter++;
+  if (userAttemptsCounter <= maxAttempts) {
+    if (event.target.id !== 'container' && event.target.id === 'left-image') {
+      Product.allProducts[leftImageIndex].votes++;
+    }
+    else if (event.target.id !== 'container' && event.target.id === 'middle-image') {
+      Product.allProducts[middleImageIndex].votes++;
+    }
+    else {
+      Product.allProducts[rightImageIndex].votes++;
+    }
+    renderThreeImages();
+  } else {
+    container.removeEventListener('click', handleUserClick);
+    let button = document.getElementById('btn').style.display ='block';
+    button=document.getElementById('btn');
+    button.addEventListener('click', showingResults);
+    // eslint-disable-next-line no-inner-declarations
+    function showingResults(event) {
+      let list = document.getElementById('results-list');
+      for (let i = 0; i < Product.allProducts.length; i++) {
+        let productResult = document.createElement('li');
+        list.appendChild(productResult);
+        productResult.textContent = `${Product.allProducts[i].name} has ${Product.allProducts[i].votes} votes, and was seen ${Product.allProducts[i].timesShown} times.`;
+        button.removeEventListener('click', showingResults);
       }
-      
-    arrOfindex[0]=leftIndex;
-    arrOfindex[1]=rightIndex;
-    arrOfindex[2]=midindex;
-    console.log(arrOfindex);
-  
-
-  
-  leftImageElement.src =  product.allImages[leftIndex].source;
-  rightImageElement.src = product.allImages[rightIndex].source;
-  midimageElemnt.src = product.allImages[midindex].source
-  product.allImages[leftIndex].time++
-  product.allImages[rightIndex].time++
-  product.allImages[midindex].time++
-
-
-}
-
-renderthreeImages();
-
-container.addEventListener('click',handleClicking);
-
-
-
-
-function handleClicking(event){
- 
-    counts++; 
-    if(maxAttempts >= counts){
-      if(event.target.id ==='left-image'){
-         product.allImages[leftIndex].votes++;
-       }else if(event.target.id ==='right-image'){
-            product.allImages[rightIndex].votes++;
-    }else if(event.target.id ==='mid-image'){
-        product.allImages[midindex].votes++;
-}
-
-
-    renderthreeImages();
-    console.log(product.allImages);
-  }else {
-    container.removeEventListener('click',handleClicking);
-
-    renderList();
-    chart()
+    }
   }
-}
-
-let arrOfVotes = [];
-let arrOfShown = [];
-function renderList(){
-  let ul = document.getElementById('unList');
-  for(let i = 0 ; i < product.allImages.length;i++){
-    arrOfVotes.push(product.allImages[i].votes);
-    arrOfShown.push(product.allImages[i].time);
-    let li = document.createElement('li');
-    ul.appendChild(li);
-    li.textContent = `${product.allImages[i].name}  has ${product.allImages[i].votes} Votes and it has appeared ${product.allImages[i].time} times`;
-  }
-
-}
-
-
-
-
-function genrateRandomIndex(){
-   return Math.floor(Math.random() * product.allImages.length); 
 
 }
